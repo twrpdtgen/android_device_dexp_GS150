5823254aaf25d5e85b2f797dbd2f3c33e054791e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-14 08:54:18 +0200

    GS150: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: dexp Model: GS150 Script version: 2.0.1 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_GS150.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_GS150-user \
+    omni_GS150-userdebug \
+    omni_GS150-eng
